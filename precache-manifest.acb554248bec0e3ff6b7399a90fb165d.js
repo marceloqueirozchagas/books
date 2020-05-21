311e01cc631b2147bbc3af0e0dc370279605e6bd
Marcelo Queiroz <smartcelo3@hotmail.com>
2020-05-21 13:56:32 -0400

    Updates

---
+++ precache-manifest.acb554248bec0e3ff6b7399a90fb165d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf6170845831a9de0594199aa6a3291a",
+    "url": "/books/index.html"
+  },
+  {
+    "revision": "fb40dccdfde51b394ed9",
+    "url": "/books/static/css/main.bde33d9a.chunk.css"
+  },
+  {
+    "revision": "4bd5b3cf809453943b20",
+    "url": "/books/static/js/2.28926c0b.chunk.js"
+  },
+  {
+    "revision": "ab6bd92c46b489bf0abe330868a5708e",
+    "url": "/books/static/js/2.28926c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb40dccdfde51b394ed9",
+    "url": "/books/static/js/main.c74e70f6.chunk.js"
+  },
+  {
+    "revision": "134824a65083827aee66",
+    "url": "/books/static/js/runtime-main.b2c4b0f3.js"
+  }
+]);
